648b9e9f4973a6b2048f2030cd66da88617c723e
Lambocrisis <baizhanji_@outlook.com>
2024-07-29 16:06:56 +0800

    test parser.py

--- dump-ks.json/parser.py
+++ dump-ks.json/parser.py
@@ -14,15 +14,34 @@ def parse(filename):
     scnLength = (len(data['scenes']))
     for texts in range(scnLength):
         try:
+        #    llnt = ""
             for i in range(len(data['scenes'][texts]['texts'])):
-                scns = data['scenes'][texts]['texts'][i][2]
-                createFile.write(scns + "\n")
+                scns = data['scenes'][texts]['texts'][i]
+        #        for r in range(len(scns)):
+        #            name = scns[r][0]
+        #            name_jp = scns[r][1][0][0]
+        #            name_en = scns[r][2][0][0]
+        #            name_cn = scns[r][3][0][0]
+        #            text_jp = scns[r][1][1][0]
+        #            text_en = scns[r][2][1][0]
+        #            text_cn = scns[r][3][1][0]
+        #            oneline = (
+        #                    f"name: {name}\n"
+        #                    f"name_jp: {name_jp}\n"
+        #                    f"text_jp: {text_jp}\n"
+        #                    f"name_en: {name_en}\n"
+        #                    f"text_en: {text_en}\n"
+        #                    f"name_cn: {name_cn}\n"
+        #                    f"text_cn: {text_cn}\n\n"
+        #                )
+        #            llnt += oneline
+                createFile.write(str(scns) + "\n")
         except KeyError:
             pass
 
 
 for filename in os.listdir():
-    if filename.endswith('.json'):
+    if filename.endswith('.ks.json'):
         parse(filename)
 print("Finished!")
 
